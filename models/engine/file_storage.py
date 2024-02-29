f31d731cfee442e6cb20e85a4872d11ca53a466b
Johnson Oragui <johnson.oragui@gmail.com>
2024-02-29 21:44:20 +0100

    modified to remove bare except

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,6 @@
 """
 Contains the FileStorage class
 """
-
 import json
 import models
 from models.amenity import Amenity
@@ -59,7 +58,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
