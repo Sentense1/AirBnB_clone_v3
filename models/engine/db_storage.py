7497c3d2ff88b04fca632d18250bff81ef29f3de
Johnson Oragui <johnson.oragui@gmail.com>
2024-03-02 10:41:27 +0100

    fixed count method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -90,8 +90,6 @@ class DBStorage:
         '''count:
         count the number of objects in storage matching the given class.
         '''
-        if cls not in classes.values():
-            return 0
         if not cls:
             data = self.all(cls)
         if cls and cls in classes.values():
